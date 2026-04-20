dbb846fdc2219e9a815ade756b652db51ec365f8
jlinero <41220138+jlinero@users.noreply.github.com>
2026-04-20 14:34:19 -0500

    Add files via upload

---
+++ imi-environments.js
@@ -0,0 +1,32 @@
+var imiEnvironments = {
+    "target": "default",
+    "default": {
+        "asset": {
+            "appId": 'WE12231025',
+            "appSecret": 'Dl2x0gSN',
+            "pathConfig": {
+                "assetPath": "/assets/",
+                "root": "/"
+            }
+        },
+        "imiclient": {
+            "shouldRequestNotificationPermission": true,
+            "config": {
+                
+            },
+            "imipush": {
+                "safariWebPushId": 'web.com.imiconnect.safari.webpush'
+            },
+            "authdomain": 'https://rtm-us.imiconnect.io/rtmsAPI',
+            "rtmsdomain": 'rtm-us.imiconnect.io',
+            "safariRegisterURL": 'https://rtm-us.imiconnect.io/apnpweb/'
+        },
+        "sw": {
+            "config": {
+                "messagingSenderId": '',
+                "serverUrl": 'https://rtm-us.imiconnect.io',
+                "appid": 'WE12231025'
+            }
+        }
+    }
+}
